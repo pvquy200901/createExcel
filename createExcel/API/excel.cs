90c337be97e46df7ab434384172abc9309dd5e45
quy <93233872+pvquy200901@users.noreply.github.com>
2023-01-04 16:41:11 +0700

    Add project files.

---
+++ createExcel/API/excel.cs
@@ -0,0 +1,24 @@
+﻿using IronXL;
+
+namespace createExcel.API
+{
+    public class excel
+    {
+        public async Task<string> addExcel(string excelFile, string sheetName, string cell, string value)
+        {
+            try
+            {
+
+                WorkBook workBook = WorkBook.Load(@"" + excelFile);
+                WorkSheet sheet = workBook.GetWorkSheet(sheetName);
+                sheet[cell].Value = value;
+                workBook.Save();
+                return "Ok";
+            }
+            catch
+            {
+                return "";
+            }
+        }
+    }
+}
---
+++ createExcel/Controllers/ExcelController.cs
@@ -0,0 +1,31 @@
+﻿using createExcel.API;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace createExcel.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExcelController : ControllerBase
+    {
+
+
+        public static excel excel = new excel();
+        [HttpPut]
+        [Route("addExcel")]
+
+        public async Task<IActionResult> addExcelAsync(string excelFile, string sheetName, string cell, string value)
+        {
+
+            string code = await excel.addExcel( excelFile,  sheetName,  cell,  value);
+            if (string.IsNullOrEmpty(code))
+            {
+                return BadRequest();
+            }
+            else
+            {
+                return Ok(code);
+            }
+        }
+    }
+}
